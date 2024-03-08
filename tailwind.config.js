2032516752dfe16feab65c5abb937778a67a81b8
Pandhu Arya <pandhu.munjalindra@gmail.com>
2024-03-08 13:46:17 +0700

    chore: finishing set up project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#5D9A9A",
+        secondary: "#579393",
+        background: "#F4FFFF",
+      }
+    },
+  },
+  plugins: [],
+};
