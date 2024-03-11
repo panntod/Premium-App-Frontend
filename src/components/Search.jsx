00a4a21b1bbb90a45bc1f58a8f209a743daf20ed
Pandhu Arya <pandhu.munjalindra@gmail.com>
2024-03-11 07:59:45 +0700

    feat: added component custom search

---
+++ src/components/Search.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { IoSearch } from "react-icons/io5";
+
+const Search = ({ search, setSearch }) => {
+  return (
+    <>
+      <IoSearch className="text-2xl text-primary" />
+      <input
+        type="search"
+        name="Search"
+        placeholder="Search"
+        className="pl-4 text-gray-500 text-lg font-medium outline-none w-full"
+        autoComplete="off"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+    </>
+  );
+};
+
+export default Search;
