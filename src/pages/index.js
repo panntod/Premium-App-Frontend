59726fb188e32b66142f278e08afa5e73dea8c13
Pandhu Arya <pandhu.munjalindra@gmail.com>
2024-03-13 09:43:00 +0700

    feat: added export default for all pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
 export { default as Login } from "./Login"
 export { default as Register } from "./Register"
+
 export { default as DashboardUser } from "./user/Dashboard"
+export { default as Profile } from "./user/Profile"
+export { default as History } from "./user/History"
+export { default as Pesan } from "./user/Pesan"
+
+export { default as Aplikasi } from "./admin/Aplikasi"
+export { default as User } from "./admin/User"
+export { default as Tier } from "./admin/Tier"
+export { default as Transaksi } from "./admin/Transaksi"
+
 export { default as NotFoundPage } from "./NotFoundPage"
 export { default as ForbiddenPage } from "./ForbiddenPage"
