9cd9a6043c57823d2292af2ca1d08044ba63c1c1
Pandhu Arya <pandhu.munjalindra@gmail.com>
2024-03-13 09:39:38 +0700

    feat: make it simply 'getAuth' and reuseable

--- src/helpers/AuthHelpers.jsx
+++ src/helpers/AuthHelpers.jsx
@@ -2,7 +2,7 @@ const SetAuth = (data) => {
   const idString = JSON.stringify(data.id);
   const roleString = JSON.stringify(data.role);
   const namaString = JSON.stringify(data.username);
-  const tokenString = data.token;
+  const tokenString = JSON.stringify(data.token);
 
   localStorage.setItem("logged", "true");
   localStorage.setItem("userID", idString);
@@ -11,46 +11,26 @@ const SetAuth = (data) => {
   localStorage.setItem("token", tokenString);
 };
 
-const GetRole = () => {
-  const roleString = localStorage.getItem("roleUser");
+const GetAuth = (key) => {
+  const itemString = localStorage.getItem(key);
 
-  if (roleString) {
-    const role = JSON.parse(roleString);
-    return role;
-  }
-
-  return null;
-};
-
-const GetUserId = () => {
-  const userId = localStorage.getItem("userID");
-
-  if (roleString) {
-    const userID = JSON.parse(userId);
-    return userID;
-  }
-
-  return null;
-};
-
-const GetLogged = () => {
-  const loginStatus = localStorage.getItem("logged");
-
-  if (loginStatus) {
-    const login = JSON.parse(loginStatus);
-    return login;
+  if (itemString) {
+    try {
+      return JSON.parse(itemString);
+    } catch (error) {
+      console.error(`Error parsing ${key} from local storage:`, error);
+    }
   }
 
   return null;
 };
 
 const ClearAuth = () => {
-  localStorage.removeItem("logged");
-  localStorage.removeItem("userID");
-  localStorage.removeItem("roleUser");
-  localStorage.removeItem("namaUser");
-  localStorage.removeItem("token");
   localStorage.clear();
 };
 
-export default { SetAuth, GetRole, ClearAuth, GetUserId, GetLogged };
+export default {
+  SetAuth,
+  GetAuth,
+  ClearAuth,
+};
