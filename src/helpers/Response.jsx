546e1508dbe2395e3f1b9a795f50a011c145fe55
Pandhu Arya <pandhu.munjalindra@gmail.com>
2024-03-13 09:40:18 +0700

    feat: if error status 401, navigated to login page

--- src/helpers/Response.jsx
+++ src/helpers/Response.jsx
@@ -1,10 +1,14 @@
 export const handleApiError = (error) => {
-    if (error.status) {
-        const status = error.status;
-        if (status === 403) {
-        window.location.href = "/forbidden";
-        } 
+  if (error.response && error.response.status) {
+    const status = error.response.status;
+    if (status === 403) {
+      window.location.href = "/forbidden";
+    } else if (status === 401) {
+      alert(error.response.data.message);
+      localStorage.clear();
+      window.location.href = "/login";
     }
+  }
 
-    return error.response;
+  return error.response;
 };
