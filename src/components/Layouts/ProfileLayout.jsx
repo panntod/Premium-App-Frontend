64ee20745498064e8a9cf96dd384c98fb69bb103
Pandhu Arya <pandhu.munjalindra@gmail.com>
2024-03-13 09:38:39 +0700

    feat: added profile layout, to implement in profile panel

---
+++ src/components/Layouts/ProfileLayout.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Sidebar from "../Sidebar";
+import { useNavigate } from "react-router-dom";
+import { IoExit } from "react-icons/io5";
+
+const ProfileLayout = ({ children }) => {
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate("/");
+  };
+
+  return (
+    <main className="h-screen bg-background flex flex-col justify-center items-center">
+      <div className="w-full md:max-w-[1260px] mx-auto">
+        <header className="flex justify-between mb-4 md:mb-12">
+          <h1 className="text-primary font-extrabold text-3xl">Lorem</h1>
+          <button onClick={handleBack}>
+            <IoExit className="text-4xl text-secondary" />
+          </button>
+        </header>
+
+        <section className="flex flex-col-reverse md:flex-row gap-4 justify-center">
+          <Sidebar />
+          {children}
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default ProfileLayout;
