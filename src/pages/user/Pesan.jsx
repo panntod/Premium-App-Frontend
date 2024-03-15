eda47137f719493990d59487eaecf1804e9fd1ab
Pandhu Arya <pandhu.munjalindra@gmail.com>
2024-03-15 09:40:38 +0700

    feat: fixing ui, implements figma design

--- src/pages/user/Pesan.jsx
+++ src/pages/user/Pesan.jsx
@@ -69,7 +69,7 @@ const Pesan = () => {
   return (
     <main className="w-full h-screen bg-background flex justify-center md:gap-12 py-12">
       <ToastContainer />
-      <section className="w-full md:w-[730px]">
+      <section className="w-full md:w-[560px]">
         {/* Application Detail */}
         <div className="md:mb-6">
           <div className="flex items-center mb-6">
@@ -94,7 +94,7 @@ const Pesan = () => {
           <div className="md:flex">
             <div className="py-3 px-5 flex gap-4">
               <div
-                className={`flex items-center gap-4 px-4 py-2 rounded-lg ${
+                className={`flex items-center h-10 w-28 px-4 py-2 rounded-lg ${
                   info ? "bg-primary text-white" : "text-gray-400"
                 } hover:cursor-pointer`}
                 onClick={() => setInfo(true)}
@@ -102,9 +102,9 @@ const Pesan = () => {
                 <a className="text-lg font-semibold">Informasi</a>
               </div>
               <div
-                className={`flex items-center gap-4 px-4 py-2 rounded-lg ${
+                className={`flex items-center h-10 w-[230px] px-4 py-2 rounded-lg ${
                   !info ? "bg-primary text-white" : "text-gray-400"
-                } hover:cursor-pointer`}
+                } hover:cursor-pointer whitespace-nowrap`}
                 onClick={() => setInfo(false)}
               >
                 <a className="text-lg font-semibold">Skema Berlangganan</a>
@@ -117,39 +117,40 @@ const Pesan = () => {
             {info ? (
               <div className={`p-4`}>
                 <p className="bg-background p-2">
-                  Paket Patungan pada <strong>{application?.nama}</strong> merupakan produk Ready. Untuk
-                  dapat berlangganan paket ini, kamu dapat langsung mendaftar
-                  dan melakukan pembayaran. Kami akan langsung proses pesanan
-                  kamu setelah pembayaran berhasil.
+                  Paket Patungan pada <strong>{application?.nama}</strong>{" "}
+                  merupakan produk Ready. Untuk dapat berlangganan paket ini,
+                  kamu dapat langsung mendaftar dan melakukan pembayaran. Kami
+                  akan langsung proses pesanan kamu setelah pembayaran berhasil.
                 </p>
               </div>
             ) : (
               <div className={`p-6 flex flex-col gap-4`}>
-                <div className="flex items-center gap-4">
+                <div className="flex items-center gap-4 w-full">
                   <span className="w-8 h-8 rounded-full flex justify-center items-center bg-secondary text-white">
                     1
                   </span>{" "}
-                  Lorem membuat akun dan membeli Paket Premium di {application?.nama}
+                  Lorem membuat akun dan membeli Paket Premium di{" "}
+                  {application?.nama}
                 </div>
-                <div className="flex items-center gap-4">
+                <div className="flex items-center gap-4 w-full">
                   <span className="w-8 h-8 rounded-full flex justify-center items-center bg-secondary text-white">
                     2
                   </span>{" "}
                   Lorem bertindak sebagai user Host dalam grup
                 </div>
-                <div className="flex items-center gap-4">
+                <div className="flex items-center gap-4 w-full">
                   <span className="w-8 h-8 rounded-full flex justify-center items-center bg-secondary text-white">
                     3
                   </span>{" "}
                   Member membuat/ menyediakan akun masing-masing
                 </div>
-                <div className="flex items-center gap-4">
+                <div className="flex items-center gap-4 w-full">
                   <span className="w-8 h-8 rounded-full flex justify-center items-center bg-secondary text-white">
                     4
                   </span>{" "}
                   Lorem mengundang 5 member untuk join 1 grup Premium
                 </div>
-                <div className="flex items-center gap-4">
+                <div className="flex items-center gap-4 w-full">
                   <span className="w-8 h-8 rounded-full flex justify-center items-center bg-secondary text-white">
                     5
                   </span>{" "}
@@ -162,95 +163,101 @@ const Pesan = () => {
       </section>
       {/* End */}
 
-      <section className="w-full md:w-[360px] p-6">
+      <section className="w-full md:w-[510px] p-6">
         <div className="p-6 flex flex-col gap-4">
           <h1 className="text-xl font-semibold">Pilih Durasi Langganan</h1>
           <div className="flex flex-col gap-4">
             {/* Radio Button */}
-            <div className="flex items-center gap-4">
-              <input
-                type="radio"
-                id="1bulan"
-                name="duration"
-                value="1bulan"
-                checked={duration === 1}
-                onChange={() => setDuration(1)}
-                className="sr-only"
-              />
-              <label
-                htmlFor="1bulan"
-                className={`flex-1 py-2 rounded-lg text-center cursor-pointer ${
-                  duration === 1
-                    ? "bg-primary text-white"
-                    : "bg-gray-200 text-gray-500"
-                }`}
-              >
-                1 Bulan
-              </label>
+
+            <div className="flex gap-4">
+              <div className="flex items-center gap-4 w-full">
+                <input
+                  type="radio"
+                  id="1bulan"
+                  name="duration"
+                  value="1bulan"
+                  checked={duration === 1}
+                  onChange={() => setDuration(1)}
+                  className="sr-only"
+                />
+                <label
+                  htmlFor="1bulan"
+                  className={`flex-1 py-2 rounded-lg text-center cursor-pointer ${
+                    duration === 1
+                      ? "bg-primary text-white"
+                      : "bg-gray-200 text-gray-500"
+                  }`}
+                >
+                  1 Bulan
+                </label>
+              </div>
+              <div className="flex items-center gap-4 w-full">
+                <input
+                  type="radio"
+                  id="3bulan"
+                  name="duration"
+                  value="3bulan"
+                  checked={duration === 3}
+                  onChange={() => setDuration(3)}
+                  className="sr-only"
+                />
+                <label
+                  htmlFor="3bulan"
+                  className={`flex-1 py-2 rounded-lg text-center cursor-pointer ${
+                    duration === 3
+                      ? "bg-primary text-white"
+                      : "bg-gray-200 text-gray-500"
+                  }`}
+                >
+                  3 Bulan
+                </label>
+              </div>
             </div>
-            <div className="flex items-center gap-4">
-              <input
-                type="radio"
-                id="3bulan"
-                name="duration"
-                value="3bulan"
-                checked={duration === 3}
-                onChange={() => setDuration(3)}
-                className="sr-only"
-              />
-              <label
-                htmlFor="3bulan"
-                className={`flex-1 py-2 rounded-lg text-center cursor-pointer ${
-                  duration === 3
-                    ? "bg-primary text-white"
-                    : "bg-gray-200 text-gray-500"
-                }`}
-              >
-                3 Bulan
-              </label>
+            <div className="flex gap-4">
+              <div className="flex items-center gap-4 w-full">
+                <input
+                  type="radio"
+                  id="6bulan"
+                  name="duration"
+                  value="6bulan"
+                  checked={duration === 6}
+                  onChange={() => setDuration(6)}
+                  className="sr-only"
+                />
+                <label
+                  htmlFor="6bulan"
+                  className={`flex-1 py-2 rounded-lg text-center cursor-pointer ${
+                    duration === 6
+                      ? "bg-primary text-white"
+                      : "bg-gray-200 text-gray-500"
+                  }`}
+                >
+                  6 Bulan
+                </label>
+              </div>
+              <div className="flex items-center gap-4 w-full">
+                <input
+                  type="radio"
+                  id="8bulan"
+                  name="duration"
+                  value="8bulan"
+                  checked={duration === 8}
+                  onChange={() => setDuration(8)}
+                  className="sr-only"
+                />
+                <label
+                  htmlFor="8bulan"
+                  className={`flex-1 py-2 rounded-lg text-center cursor-pointer ${
+                    duration === 8
+                      ? "bg-primary text-white"
+                      : "bg-gray-200 text-gray-500"
+                  }`}
+                >
+                  8 Bulan
+                </label>
+              </div>
             </div>
-            <div className="flex items-center gap-4">
-              <input
-                type="radio"
-                id="6bulan"
-                name="duration"
-                value="6bulan"
-                checked={duration === 6}
-                onChange={() => setDuration(6)}
-                className="sr-only"
-              />
-              <label
-                htmlFor="6bulan"
-                className={`flex-1 py-2 rounded-lg text-center cursor-pointer ${
-                  duration === 6
-                    ? "bg-primary text-white"
-                    : "bg-gray-200 text-gray-500"
-                }`}
-              >
-                6 Bulan
-              </label>
-            </div>
-            <div className="flex items-center gap-4">
-              <input
-                type="radio"
-                id="8bulan"
-                name="duration"
-                value="8bulan"
-                checked={duration === 8}
-                onChange={() => setDuration(8)}
-                className="sr-only"
-              />
-              <label
-                htmlFor="8bulan"
-                className={`flex-1 py-2 rounded-lg text-center cursor-pointer ${
-                  duration === 8
-                    ? "bg-primary text-white"
-                    : "bg-gray-200 text-gray-500"
-                }`}
-              >
-                8 Bulan
-              </label>
-            </div>
+
             {/* End Radio Button */}
 
             {/* Total Section */}
