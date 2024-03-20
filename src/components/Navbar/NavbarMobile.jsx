109ae4c2c510d70c1a544d2e156f22cb252d7d78
panntod <pandhu.munjalindra@gmail.com>
2024-03-21 05:41:24 +0700

    feat: added background collor

--- src/components/Navbar/NavbarMobile.jsx
+++ src/components/Navbar/NavbarMobile.jsx
@@ -2,7 +2,7 @@ import CustomButton from "../Button";
 
 const NavbarMobile = ({ logged, handleLogin, handleLogout }) => {
   return (
-    <div className="md:hidden fixed top-20 w-full bg-background p-4">
+    <div className="md:hidden fixed top-20 w-full bg-transparent backdrop-blur-lg p-4 z-50">
       <ul className="flex flex-col space-y-4">
         <li>
           <a href="" className="text-xl font-medium text-primary">
