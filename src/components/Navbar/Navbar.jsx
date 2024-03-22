6e30fffd25cbb480d8129c10f13330e67b0f72fe
panntod <pandhu.munjalindra@gmail.com>
2024-03-22 15:48:18 +0700

    feat: added animation when navbar in front of section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,6 +8,7 @@ const Navbar = () => {
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
   const [logged, setLogged] = useState(false);
   const [role, setRole] = useState("");
+  const [scrolled, setScrolled] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -42,15 +43,33 @@ const Navbar = () => {
     }
   };
 
+  useEffect(() => {
+    function handleScroll() {
+      let scrollPosition = window.scrollY;
+      const sectionPosition = document.getElementById('pengguna').offsetTop;
+      const navbarHeight = document.querySelector('header').offsetHeight;
+      if (scrollPosition >= sectionPosition - navbarHeight && scrollPosition <= 1600 ) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <>
       <header className="w-full fixed top-0 h-24 flex justify-between items-center px-4 md:px-8 lg:px-16 bg-transparent backdrop-blur-xl z-50">
-        <h1 className="text-primary font-extrabold text-3xl">Lorem</h1>
+        <h1 className={`${scrolled ? "text-white" : "text-primary"} font-extrabold text-3xl`}>Lorem</h1>
         <div className="hidden md:flex space-x-12 items-center">
           <ul className="flex space-x-12 items-center">
             <li className="my-6">
               <a
-                className="text-xl hover:text-primary-dark cursor-pointer font-semibold duration-500 text-primary"
+                className={`text-xl cursor-pointer font-semibold duration-500 ${scrolled ? "hover:text-slate-200 text-white" : "hover:text-primary-dark text-primary"}`}
                 onClick={(e) => {
                   e.preventDefault();
                   handleScrollToSection("layanan");
@@ -61,7 +80,7 @@ const Navbar = () => {
             </li>
             <li className="my-6">
               <a
-                className="text-xl hover:text-primary-dark cursor-pointer font-semibold duration-500 text-primary"
+                className={`text-xl cursor-pointer font-semibold duration-500 ${scrolled ? "hover:text-slate-200 text-white" : "hover:text-primary-dark text-primary"}`}
                 onClick={(e) => {
                   e.preventDefault();
                   handleScrollToSection("pengguna");
@@ -72,7 +91,18 @@ const Navbar = () => {
             </li>
             <li className="my-6">
               <a
-                className="text-xl hover:text-primary-dark cursor-pointer font-semibold duration-500 text-primary"
+                className={`text-xl cursor-pointer font-semibold duration-500 ${scrolled ? "hover:text-slate-200 text-white" : "hover:text-primary-dark text-primary"}`}
+                onClick={(e) => {
+                  e.preventDefault();
+                  handleScrollToSection("benefit");
+                }}
+              >
+                Benefit
+              </a>
+            </li>
+            <li className="my-6">
+              <a
+                className={`text-xl cursor-pointer font-semibold duration-500 ${scrolled ? "hover:text-slate-200 text-white" : "hover:text-primary-dark text-primary"}`}
                 onClick={(e) => {
                   e.preventDefault();
                   handleScrollToSection("caraPesan");
@@ -85,7 +115,7 @@ const Navbar = () => {
             {role === "admin" ? (
               <li className="my-6">
                 <a
-                  className="text-xl hover:text-primary-dark cursor-pointer font-semibold duration-500 text-primary"
+                  className={`text-xl cursor-pointer font-semibold duration-500 ${scrolled ? "hover:text-slate-200 text-white" : "hover:text-primary-dark text-primary"}`}
                   href="/admin/aplikasi"
                 >
                   Admin Panel
@@ -100,7 +130,7 @@ const Navbar = () => {
             <CustomDropdown handleLogout={handleLogout} />
           ) : (
             <CustomButton
-              className="bg-gradient-to-tr from-primary to-secondary font-bold text-white w-[150px]"
+              className="bg-gradient-to-tr from-primary to-secondary text-white w-[150px]"
               type="button"
               onClick={handleLogin}
             >
