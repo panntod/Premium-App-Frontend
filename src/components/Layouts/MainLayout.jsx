e252c21c730c038dcb8c43b83616eb31f45209dd
panntod <pandhu.munjalindra@gmail.com>
2024-03-22 15:47:38 +0700

    refactor: change padding size

--- src/components/Layouts/MainLayout.jsx
+++ src/components/Layouts/MainLayout.jsx
@@ -4,7 +4,7 @@ const MainLayout = ({ children }) => {
   return (
     <>
       <Navbar />
-      <main className="px-7 md:px-14 pt-14 min-h-screen bg-background pb-16 md:pb-24 overflow-hidden">
+      <main className="px-8 md:px-0 pt-14 min-h-screen bg-background pb-16 md:pb-24 overflow-hidden">
         {children}
       </main>
       <Footer />
