c93db3d6a0f6b31d57d0f2468126a561e332276f
panntod <pandhu.munjalindra@gmail.com>
2024-04-01 09:08:04 +0700

    feat: Create loading user interface

---
+++ src/components/Loading.jsx
@@ -0,0 +1,11 @@
+const Loading = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="rounded-full h-6 w-6 bg-primary-dark animate-ping"></div>
+      <div className="rounded-full h-8 w-8 bg-primary-dark animate-ping"></div>
+      <div className="rounded-full h-6 w-6 bg-primary-dark animate-ping"></div>
+    </div>
+  );
+};
+
+export default Loading;
--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ export { default as NavbarAdmin } from "./Navbar/NavbarAdmin";
 export { default as NavbarMobile } from "./Navbar/NavbarMobile";
 export { default as Sidebar } from "./Sidebar";
 export { default as Footer } from "./Footer";
+export { default as Loading } from "./Loading";
 
 export { default as CustomDropdown } from "./Dropdown";
 export { default as CustomButton } from "./Button";
