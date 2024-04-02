93fa269a791bd38fb1b043bb202c4c3166949a09
panntod <pandhu.munjalindra@gmail.com>
2024-04-02 09:49:56 +0700

    refactor: Change default export

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
 export { default as Login } from "./Login";
 export { default as Register } from "./Register";
 
-export { default as DashboardUser } from "./user/Dashboard";
-export { default as Profile } from "./user/Profile";
-export { default as History } from "./user/History";
-export { default as Pesan } from "./user/Pesan";
+export { default as DashboardUser } from "./Dashboard";
+export { default as Profile } from "./Profile";
+export { default as History } from "./History";
+export { default as Order } from "./Order";
 
-export { default as Aplikasi } from "./admin/Aplikasi";
-export { default as User } from "./admin/User";
-export { default as Transaksi } from "./admin/Transaksi";
+export { default as Aplikasi } from "./Application";
+export { default as User } from "./User";
+export { default as Transaksi } from "./Transaction";
 
 export { default as NotFoundPage } from "./NotFoundPage";
 export { default as ErrorPage } from "./ErrorPage";
