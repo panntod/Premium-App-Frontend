dfc83de74dc1877cb5f623c57f8f57c0b4457170
panntod <pandhu.munjalindra@gmail.com>
2024-04-02 09:48:08 +0700

    feat: Added initial toast container on app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import Router from "./routes/Router";
+import { ToastContainer } from "react-toastify";
 import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
       <Router />
     </BrowserRouter>
   );
