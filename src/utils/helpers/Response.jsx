8de7d90dba4447e7b7d800bda73081aafc1c0793
panntod <pandhu.munjalindra@gmail.com>
2024-04-02 09:50:18 +0700

    feat: Added hanfle response and handle back home

--- src/utils/helpers/Response.jsx
+++ src/utils/helpers/Response.jsx
@@ -1,15 +1,41 @@
+import { toast } from "react-toastify";
+
 export const handleApiError = (error) => {
   if (error.response && error.response.status) {
     const status = error.response.status;
     if (status === 403) {
       window.location.href = "/forbidden";
     } else if (status === 401) {
-      alert(error.response.data.message);
-      localStorage.clear();
-      window.location.href = "/login";
+      toast.error(error.response.data.message, { autoClose: 2000 });
+      setTimeout(() => {
+        localStorage.clear();
+        window.location.href = "/login";
+      }, 2000);
     }
   } else if (error.message === "Network Error") {
     window.location.href = "/error";
   }
   return error.response;
+};
+
+export const handleApiResponse = (
+  response,
+  additionalSuccessAction = () => {},
+) => {
+  if (response.success === true) {
+    toast.success(response.message, { autoClose: 2000 });
+    additionalSuccessAction();
+  } else {
+    const errorMessage = response.data.errors
+      ? response.data.errors
+      : ["Terjadi kesalahan pada server"];
+
+    errorMessage.forEach((message) => {
+      toast.error(message, { autoClose: 2000 });
+    });
+  }
+};
+
+export const handleBackHome = () => {
+  window.location.href = "/";
 };
