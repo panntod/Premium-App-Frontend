0f7c75f406f23f4ef9dfc1d171e96561cc2ccf53
panntod <pandhu.munjalindra@gmail.com>
2024-04-02 09:48:20 +0700

    feat: Added animation on hover

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import { Button } from "@material-tailwind/react";
 const CustomButton = ({ children, className, loading, onClick, type }) => {
   return (
     <Button
-      className={`${loading ? "opacity-50 cursor-not-allowed" : ""} ${className} text-xs font-semibold rounded-full whitespace-nowrap`}
+      className={`${loading ? "opacity-50 cursor-not-allowed" : ""} ${className} hover:scale-105 text-xs font-semibold rounded-full whitespace-nowrap`}
       type={type}
       disabled={loading}
       onClick={onClick}
