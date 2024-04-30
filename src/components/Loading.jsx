5b34d5d3f0a52030f49b71db99f81d043c66d27a
panntod <pandhu.munjalindra@gmail.com>
2024-04-30 11:38:02 +0700

    refactor: Create lazy load function

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,6 +1,6 @@
 const Loading = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-screen bg-background">
       <div className="rounded-full h-6 w-6 bg-primary-dark animate-ping"></div>
       <div className="rounded-full h-8 w-8 bg-primary-dark animate-ping"></div>
       <div className="rounded-full h-6 w-6 bg-primary-dark animate-ping"></div>
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,86 +1,89 @@
+import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import ProtectRoute from "./ProtectedRoute";
-import {
-  Login,
-  Register,
-  DashboardUser,
-  NotFoundPage,
-  ForbiddenPage,
-  Aplikasi,
-  User,
-  Transaksi,
-  Profile,
-  History,
-  Order,
-  ErrorPage,
-} from "../pages/";
 import AdminRoute from "./AdminRoute";
+import LoadingSpinner from "../components/Loading";
+
+const Login = React.lazy(() => import("../pages/Login"));
+const Register = React.lazy(() => import("../pages/Register"));
+const DashboardUser = React.lazy(() => import("../pages/Dashboard"));
+const NotFoundPage = React.lazy(() => import("../pages/NotFoundPage"));
+const ForbiddenPage = React.lazy(() => import("../pages/ForbiddenPage"));
+const Aplikasi = React.lazy(() => import("../pages/Application"));
+const User = React.lazy(() => import("../pages/User"));
+const Transaksi = React.lazy(() => import("../pages/Transaction"));
+const Profile = React.lazy(() => import("../pages/Profile"));
+const History = React.lazy(() => import("../pages/History"));
+const Order = React.lazy(() => import("../pages/Order"));
+const ErrorPage = React.lazy(() => import("../pages/ErrorPage"));
 
 const Router = () => {
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/" element={<DashboardUser />} />
+    <Suspense fallback={<LoadingSpinner />}>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<DashboardUser />} />
 
-      <Route
-        path="/profile"
-        element={
-          <ProtectRoute>
-            <Profile />
-          </ProtectRoute>
-        }
-      />
+        <Route
+          path="/profile"
+          element={
+            <ProtectRoute>
+              <Profile />
+            </ProtectRoute>
+          }
+        />
 
-      <Route
-        path="/pesanan"
-        element={
-          <ProtectRoute>
-            <History />
-          </ProtectRoute>
-        }
-      />
+        <Route
+          path="/pesanan"
+          element={
+            <ProtectRoute>
+              <History />
+            </ProtectRoute>
+          }
+        />
 
-      <Route
-        path="/pesan"
-        element={
-          <ProtectRoute>
-            <Order />
-          </ProtectRoute>
-        }
-      />
+        <Route
+          path="/pesan"
+          element={
+            <ProtectRoute>
+              <Order />
+            </ProtectRoute>
+          }
+        />
 
-      <Route
-        path="/admin/aplikasi"
-        element={
-          <AdminRoute>
-            <Aplikasi />
-          </AdminRoute>
-        }
-      />
+        <Route
+          path="/admin/aplikasi"
+          element={
+            <AdminRoute>
+              <Aplikasi />
+            </AdminRoute>
+          }
+        />
 
-      <Route
-        path="/admin/user"
-        element={
-          <AdminRoute>
-            <User />
-          </AdminRoute>
-        }
-      />
+        <Route
+          path="/admin/user"
+          element={
+            <AdminRoute>
+              <User />
+            </AdminRoute>
+          }
+        />
 
-      <Route
-        path="/admin/transaksi"
-        element={
-          <AdminRoute>
-            <Transaksi />
-          </AdminRoute>
-        }
-      />
+        <Route
+          path="/admin/transaksi"
+          element={
+            <AdminRoute>
+              <Transaksi />
+            </AdminRoute>
+          }
+        />
 
-      <Route path="/error" element={<ErrorPage />} />
-      <Route path="/forbidden" element={<ForbiddenPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+        <Route path="/error" element={<ErrorPage />} />
+        <Route path="/forbidden" element={<ForbiddenPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Suspense>
   );
 };
 
