20b7f64183f6f1c4eb215c4545acd0cba166e39b
panntod <pandhu.munjalindra@gmail.com>
2024-04-30 12:19:59 +0700

    refactor: Destructuring react lazy load

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,21 +1,21 @@
-import React, { Suspense } from "react";
+import { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 import ProtectRoute from "./ProtectedRoute";
 import AdminRoute from "./AdminRoute";
 import LoadingSpinner from "../components/Loading";
 
-const Login = React.lazy(() => import("../pages/Login"));
-const Register = React.lazy(() => import("../pages/Register"));
-const DashboardUser = React.lazy(() => import("../pages/Dashboard"));
-const NotFoundPage = React.lazy(() => import("../pages/NotFoundPage"));
-const ForbiddenPage = React.lazy(() => import("../pages/ForbiddenPage"));
-const Aplikasi = React.lazy(() => import("../pages/Application"));
-const User = React.lazy(() => import("../pages/User"));
-const Transaksi = React.lazy(() => import("../pages/Transaction"));
-const Profile = React.lazy(() => import("../pages/Profile"));
-const History = React.lazy(() => import("../pages/History"));
-const Order = React.lazy(() => import("../pages/Order"));
-const ErrorPage = React.lazy(() => import("../pages/ErrorPage"));
+const Login = lazy(() => import("../pages/Login"));
+const Register = lazy(() => import("../pages/Register"));
+const DashboardUser = lazy(() => import("../pages/Dashboard"));
+const NotFoundPage = lazy(() => import("../pages/NotFoundPage"));
+const ForbiddenPage = lazy(() => import("../pages/ForbiddenPage"));
+const Aplikasi = lazy(() => import("../pages/Application"));
+const User = lazy(() => import("../pages/User"));
+const Transaksi = lazy(() => import("../pages/Transaction"));
+const Profile = lazy(() => import("../pages/Profile"));
+const History = lazy(() => import("../pages/History"));
+const Order = lazy(() => import("../pages/Order"));
+const ErrorPage = lazy(() => import("../pages/ErrorPage"));
 
 const Router = () => {
   return (
