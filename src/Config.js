65d66f0f95f86e17c2b06de30ebab6395b411d1c
panntod <munjalindra.pandhu@gmail.com>
2026-09-23 12:38:01 +0700

    add env example and implement it

--- src/Config.js
+++ src/Config.js
@@ -1,14 +1,21 @@
 import AuthHelpers from "./utils/helpers/AuthHelpers";
 
+const baseURL = import.meta.env.VITE_API_URL;
+
 export const config = () => {
   const myToken = AuthHelpers.GetAuth("tokenUser");
+
   return {
-    headers: { Authorization: `Bearer ${myToken}` },
+    headers: {
+      Authorization: `Bearer ${myToken}`,
+    },
   };
 };
 
-export const baseURL = "http://localhost:8000";
-export const imageURL = baseURL + "/images/";
+export { baseURL };
+
+export const imageURL = `${baseURL}/images/`;
+
 export const initialRegisterState = {
   nama: "",
   username: "",
@@ -17,17 +24,20 @@ export const initialRegisterState = {
   showPassword: false,
   showConfirmPassword: false,
 };
+
 export const initialLoginState = {
   username: "",
   password: "",
   showPassword: false,
 };
+
 export const initialNewAplikasiState = {
   nama: "",
   harga: 0,
   deskripsi: "",
   image: null,
 };
+
 export const initialNewUserState = {
   username: "",
   nama: "",
